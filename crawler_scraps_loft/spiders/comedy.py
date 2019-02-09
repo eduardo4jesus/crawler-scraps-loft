fcf34e393b73e573518fd602f6867dd2b707715e
Eduardo Reis <eduardo4jesus@gmail.com>
2019-02-08 23:18:57 -0500

    Crawler for Scraps from the Loft

---
+++ crawler_scraps_loft/spiders/comedy.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ComedySpider(scrapy.Spider):
+    name = 'comedy'
+    allowed_domains = ['scrapsfromtheloft.com']
+    start_urls = ['https://scrapsfromtheloft.com/blog/']
+
+    def parse(self, response):
+        scraps = response.css('article').css('h2 > a::attr(href)').extract()
+        for scrap_url in scraps:
+            yield scrapy.Request(url=scrap_url, callback=self.parse_scrap)
+
+    def parse_scrap(self, response):
+        article = response.css('article')
+        title = article.css('div > h1::text').extract_first()
+        meta = response.css('div.fusion-meta-info-wrapper')
+        date_str = meta.css('span')[3].css('::text').extract_first()
+        categories = meta.xpath('a//text()').getall()
+        tags = meta.css('span.meta-tags > a::text').getall()
+        transcript = article.css('div.post-content > p::text').extract()
+        url = response.request.url
+
+        item = {
+            'title': title,
+            'date_str': date_str,
+            'categories': categories,
+            'tags': tags,
+            'transcript': transcript,
+            'url': url
+        }
+
+        yield item
